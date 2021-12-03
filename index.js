469817c5fa49457ea3f66c559a647a2ba9e84dad
mirulnorazmi <mmiirul00@gmail.com>
2021-12-04 00:22:21 +0800

    next commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.json('Hello world');
 })
 
+app.get('/.well-known/pki-validation/ ', (req, res, next) => {
+  res.json('test');
+})
+
 app.listen(PORT || process.env.PORT , () => {
   console.log('App runing on port : ', PORT);
 });
